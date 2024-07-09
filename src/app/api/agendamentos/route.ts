4c5071bb921b32558272e174307094478166b15e
Ronan Salvador <ronansalvador@yahoo.com.br>
2024-07-09 09:53:35 -0300

    feat: adicionando o metodo DELETE

--- src/app/api/agendamentos/route.ts
+++ src/app/api/agendamentos/route.ts
@@ -40,3 +40,23 @@ export async function POST(req: Request) {
     )
   }
 }
+
+export async function DELETE(req: Request) {
+  console.log('DELETE passou aqui')
+  try {
+    const { id } = await req.json()
+    const agendamento = await prisma.agendamento.delete({
+      where: { id: Number(id) },
+    })
+    console.log('agendamento deletado', agendamento)
+    return NextResponse.json({ agendamento })
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: 'error',
+        error,
+      },
+      { status: 500 },
+    )
+  }
+}
