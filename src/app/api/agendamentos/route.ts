61594b64d39962e5ff3d6c90511bd70e5b9d2752
Ronan Salvador <ronansalvador@yahoo.com.br>
2024-07-09 10:46:54 -0300

    feat: adicionado interface

--- src/app/api/agendamentos/route.ts
+++ src/app/api/agendamentos/route.ts
@@ -1,6 +1,19 @@
 import prisma from '../../lib/prisma'
 import { NextResponse } from 'next/server'
 
+interface Agendamento {
+  cliente: string
+  date: string
+  servico: string
+}
+
+interface UpdateAgendamento {
+  id: number
+  cliente: string
+  date: string
+  servico?: string
+}
+
 export async function GET(req: Request) {
   try {
     const agendamentos = await prisma.agendamento.findMany()
@@ -20,16 +33,16 @@ export async function GET(req: Request) {
 export async function POST(req: Request) {
   console.log('POST passou aqui')
   try {
-    const { cliente, data, servico } = await req.json()
+    const { cliente, date, servico }: Agendamento = await req.json()
     const agendamento = await prisma.agendamento.create({
       data: {
         cliente,
-        data: new Date(data),
+        data: new Date(date),
         servico,
       },
     })
     console.log('agendamento', agendamento)
-    return Response.json({ agendamento })
+    return NextResponse.json({ agendamento })
   } catch (error) {
     return NextResponse.json(
       {
@@ -44,7 +57,7 @@ export async function POST(req: Request) {
 export async function DELETE(req: Request) {
   console.log('DELETE passou aqui')
   try {
-    const { id } = await req.json()
+    const { id }: { id: number } = await req.json()
     const agendamento = await prisma.agendamento.delete({
       where: { id: Number(id) },
     })
@@ -64,12 +77,12 @@ export async function DELETE(req: Request) {
 export async function PUT(req: Request) {
   console.log('PUT passou aqui')
   try {
-    const { id, cliente, data, servico } = await req.json()
+    const { id, cliente, date, servico }: UpdateAgendamento = await req.json()
     const agendamento = await prisma.agendamento.update({
       where: { id: Number(id) },
       data: {
         cliente,
-        data: new Date(data),
+        data: new Date(date),
         servico,
       },
     })
@@ -89,14 +102,14 @@ export async function PUT(req: Request) {
 export async function PATCH(req: Request) {
   console.log('PATCH passou aqui')
   try {
-    const { id, cliente, data, servico } = await req.json()
-    const updateData = {}
+    const { id, cliente, date, servico }: UpdateAgendamento = await req.json()
+    const updateData: Partial<Agendamento> = {}
 
     if (cliente !== undefined) {
       updateData.cliente = cliente
     }
-    if (data !== undefined) {
-      updateData.data = new Date(data)
+    if (date !== undefined) {
+      updateData.date = new Date(date).toISOString()
     }
     if (servico !== undefined) {
       updateData.servico = servico
