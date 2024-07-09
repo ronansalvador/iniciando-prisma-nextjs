85953b9b3651316f70dc6208e8eef289bf2a3b3c
Ronan Salvador <ronansalvador@yahoo.com.br>
2024-07-09 09:54:05 -0300

    feat: adicionando o metodo PUT

--- src/app/api/agendamentos/route.ts
+++ src/app/api/agendamentos/route.ts
@@ -60,3 +60,28 @@ export async function DELETE(req: Request) {
     )
   }
 }
+
+export async function PUT(req: Request) {
+  console.log('PUT passou aqui')
+  try {
+    const { id, cliente, data, servico } = await req.json()
+    const agendamento = await prisma.agendamento.update({
+      where: { id: Number(id) },
+      data: {
+        cliente,
+        data: new Date(data),
+        servico,
+      },
+    })
+    console.log('agendamento atualizado', agendamento)
+    return NextResponse.json({ agendamento })
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: 'error',
+        error,
+      },
+      { status: 500 },
+    )
+  }
+}
